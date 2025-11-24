819f013741034af7d3647d95267d93d64cdf4d2b
Tran Thanh Binh <tranbinh081007@gmail.com>
2025-11-24 15:33:43 +0700

    DB updated!

--- src/config/database-config.js
+++ src/config/database-config.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    // host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true, // Bắt buộc SSL cho Neon
+            rejectUnauthorized: false // Chấp nhận chứng chỉ tự ký (thường cần thiết cho các cloud DB)
+        }
+    },
     pool: {
-        max: 100,
+        max: 20,
         min: 0,
         acquire: 30000,
         idle: 10000
