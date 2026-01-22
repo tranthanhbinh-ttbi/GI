d35da55dfb9fafba735f6236ebfa6e1888538c4b
Tran Thanh Binh <tranbinh081007@gmail.com>
2026-01-22 09:13:54 +0700

    updated series

--- src/public/js/post.js
+++ src/public/js/post.js
@@ -176,10 +176,13 @@ document.addEventListener('DOMContentLoaded', function () {
         const fullStars = Math.floor(avg);
         const hasHalf = (avg - fullStars) >= 0.5;
 
+        const emptyStarSVG = '<span class="star"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="transparent" stroke="#f6ad55" stroke-width="1" style="overflow: visible;" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg></span>';
+        const fullStarSVG = '<span class="star filled"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#f6ad55" stroke="none" style="overflow: visible;" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg></span>';
+
         for (let i = 1; i <= 5; i++) {
-            if (i <= fullStars) starsHTML += '<span class="star filled">★</span>';
+            if (i <= fullStars) starsHTML += fullStarSVG;
             else if (i === fullStars + 1 && hasHalf) starsHTML += '<span class="star half">★</span>';
-            else starsHTML += '<span class="star">☆</span>';
+            else starsHTML += emptyStarSVG;
         }
         starsContainer.innerHTML = starsHTML;
     }
